d041aa3cfff61e828ac2f441db6cd946e0d4caf0
Manondev11 <manon.elm@hotmail.com>
2026-02-17 11:51:38 +0100

    header

---
+++ homePage.js
@@ -0,0 +1,21 @@
+const navSlide = () => {
+    const burger =
+    document.querySelector('.burger');
+    const nav =
+    document.querySelector('.navLinks');
+    const allLinks =
+    document.querySelectorAll('.navLinks li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('navActive');
+        allLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = '';
+            }else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        burger.classList.toggle('toggle');
+});
+}
+navSlide();
