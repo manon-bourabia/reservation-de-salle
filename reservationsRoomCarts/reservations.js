211c64f775ed012637060680b62713ba9d3d6aeb
isabel64450 <isa.dumas64@gmail.com>
2026-02-18 14:21:16 +0100

    add the header and footer

--- reservationsRoomCarts/reservations.js
+++ reservationsRoomCarts/reservations.js
@@ -5,7 +5,24 @@ const container = document.getElementById("container")
 const template = document.getElementById("templateReservation")
 
 
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".navLinks");
+  const allLinks = document.querySelectorAll(".navLinks li");
 
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("navActive");
+    allLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+};
+navSlide();
 
 
 
